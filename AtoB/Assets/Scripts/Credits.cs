532d62e0f463d6bc632f299f1d6d16949af447d1
Vlad Limbean <vlli@itu.dk>
2017-01-21 19:29:27 +0100

    Added assets and initial Level

---
+++ AtoB/Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    public TextAsset textAsset;
+    public string allText;
+    
+    // Use this for initialization
+    void Start () {
+        string allText = textAsset.text;
+
+	}
+
+    public string getCredits(string allText)
+    {
+        return allText;
+    }
+}
